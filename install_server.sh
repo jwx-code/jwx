26894127e1de94fc21680eec899b6a3dfa1dde2e
jwx-code <aufbau.pita-0y@icloud.com>
2025-08-20 14:38:52 +0200

    set up server update web+sql 2

--- install_server.sh
+++ install_server.sh
@@ -1,7 +1,10 @@
 #
 //  install_server.sh
 //  jwx
-//
+//  SQL /Users/jonaswrede/Documents/JWX_DEV/jwx/sql/sh/setup_multipass_sql.sh
+//  WEB /Users/jonaswrede/Documents/JWX_DEV/jwx/web/sh/setup_multipass_web.sh
 //  Created by Jonas wrede on 15.08.25.
 //
 
+sh  /Users/jonaswrede/Documents/JWX_DEV/jwx/web/sh/setup_multipass_web.sh
+sh /Users/jonaswrede/Documents/JWX_DEV/jwx/sql/sh/setup_multipass_sql.sh
