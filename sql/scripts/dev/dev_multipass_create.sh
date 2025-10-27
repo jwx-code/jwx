8b2342b7ebf9658c9a7b8024a174de5b1e169edd
jwx-code <239347283+jnswde-ctrl@users.noreply.github.com>
2025-10-27 22:51:28 +0100

    Update dev_multipass_create.sh
    delete comments

--- sql/scripts/dev/dev_multipass_create.sh
+++ sql/scripts/dev/dev_multipass_create.sh
@@ -6,8 +6,6 @@ if [ -z "$1" ]; then
   echo "  instanceName : required string parameter (dev_multipass_create.sh)"
   exit 1
 else
-echo "2"
-echo $1
     VM_NAME="$1"
     VM_MEM="2G"
     VM_DISK="10G"
@@ -15,15 +13,13 @@ echo $1
     VM_CLOUD_INIT=" "
     SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "$2"
     if [ "$2" == "--sql" ]; then
           VM_CLOUD_INIT=$SCRIPT_DIR"/sql.yaml"
     else
-        echo "error - invalid server type (dev_multipass_create.sh)"
+        echo "custom error - invalid server type (dev_multipass_create.sh)"
         exit 1;
     fi
     
-echo "$VM_NAME"
-    echo "multipass starts now  (dev_multipass_create.sh)"
+    echo "multipass install now  (dev_multipass_create.sh)"
     multipass launch 24.04 --name "$VM_NAME" --mem "$VM_MEM" --disk "$VM_DISK" --cpus "$VM_CPUS" --cloud-init "$VM_CLOUD_INIT"
 fi
